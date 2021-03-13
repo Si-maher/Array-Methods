4e50c6e4b93f91e8d2ceafaa513e731195f1c602
Simon Maher <simonmaher8307@hotmail.com>
2021-03-13 16:45:45 +0200

    added first array methods

---
+++ index.js
@@ -0,0 +1,27 @@
+let stringOne = 'Only the dead have seen war'
+let stringTwo = 'You must be the change you wish to see in the world'
+
+// charAt()
+console.log(stringOne.charAt(1));
+// charCodeAt()
+console.log(stringTwo.charCodeAt(2));
+// concat()
+console.log(stringOne.concat(stringTwo));
+// endsWith()
+console.log(stringTwo.endsWith('world'));
+// fromCharCode()
+console.log(String.fromCharCode(114));
+// includes()
+console.log(stringOne.includes('the'));
+//indexOf 
+console.log(stringTwo.indexOf('must'));
+// 
+console.log();
+// 
+console.log();
+// 
+console.log();
+// 
+console.log();
+// 
+console.log();
